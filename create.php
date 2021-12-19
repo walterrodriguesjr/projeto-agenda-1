8ec9960e8dbe028dfce8eb2edb097b6a450ae01d
walter rodrigues junior <walterrjr.86@gmail.com>
2021-12-19 09:57:33 -0300

    atualizando

---
+++ create.php
@@ -0,0 +1,10 @@
+<?php
+include_once "templates/header.php";
+?>
+    <h1>Criando Contato</h1>
+    
+
+<?php
+include_once "templates/footer.php";
+?>
+
---
+++ templates/header.php
@@ -0,0 +1,37 @@
+<?php
+include_once "config/url.php";
+include_once "config/process.php";
+
+//limpa a mensagem
+if (isset($_SESSION['msg'])) {
+    $primaryMsg = $_SESSION['msg'];
+    $_SESSION['msg'] = '';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?=$BASE_URL?>css/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="<?=$BASE_URL?>index.php">
+        <img src="<?=$BASE_URL?>img/logo.svg" alt="Agenda">
+    </a>
+    <div class="navbar-nav">
+        <a href="<?=$BASE_URL?>index.php" class="nav-link active" id="home-link">Agenda</a>
+        <a href="<?=$BASE_URL?>create.php" class="nav-link active" id="home-link">Adicionar Contato</a>
+    </div>
+        </nav>
+    </header>
